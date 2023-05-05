16cb750da83453a84c25a35bd47570403c2ddd6f
LeLouLamb <112926148+LeLouLamb@users.noreply.github.com>
2023-05-05 12:28:35 -0600

    grid stuff

--- grid_class.cs
+++ grid_class.cs
@@ -1,15 +1,37 @@
-
-// C# program to print Hello World!
 using System;
   
-// namespace declaration
 namespace TicTacToe {
-  
-// Class declaration
-class Geeks {
- static void Main(string[] args){
-  Console.WriteLine("Hello World!");
-  Console.ReadKey();
+ class Grid {
+  public char[,] grid;
+  public int size;
+
+  public Grid(int size){
+   this.size = size;
+   grid = new char[size,size];
+  }
+
+  public char this[int index1,int index2]{
+   get{
+    return grid[index1,index2];
+   }
+   set{
+    grid[index1,index2] = value;
+   }
+  }
+
+  static void Main(string[] args){
+   Grid grid = new Grid(3);
+   char duff = 'o';
+   for (int i = 0; i < grid.size; i++){
+    for (int j = 0; j < grid.size; j++){
+     grid[i,j] = duff;
+     if (duff == 'x'){
+      duff = 'x';
+     } else {
+      duff = 'o';
+     }
+    }
+   }
+  }
  }
-}
 }
