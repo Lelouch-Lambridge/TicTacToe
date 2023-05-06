3253603e7a629f460a024c9dd01f66ee1dcc2d82
LeLouLamb <112926148+LeLouLamb@users.noreply.github.com>
2023-05-05 23:39:01 -0600

    Seperate game class

--- grid_class.cs
+++ grid_class.cs
@@ -8,47 +8,41 @@ namespace TicTacToe {
   public Grid(int size){
    this.size = size;
    this.grid = new char[size,size];
-   this.clear();
+   clear();
   }
 
-  private char this[int index1,int index2]{
+  public char this[int index1,int index2]{
    get{
-    return this.grid[index1,index2];
+    return grid[index1,index2];
    }
    set{
-    this.grid[index1,index2] = value;
+    grid[index1,index2] = value;
    }
   }
 
-  public void clear(){
+  public void turn(char ch, int r, int c){
+   this.grid[r,c] = ch;
+  }
+
+  private void clear(){
    for (int r = 0; r < size; r++){
     for (int c = 0; c < size; c++){
-     this.grid[r,c] = '\0';
+     grid[r,c] = '\0';
     }
    }
-  }
-
-  static void Main(string[] args){
-   Grid grid = new Grid(3);
-   System.Console.WriteLine(grid[1,1]);
-   x_Player p1 = x_Player.Instance;
-   System.Console.WriteLine(p1.letter);
-   y_Player p2 = y_Player.Instance;
-   System.Console.WriteLine(p2.letter);
   }
  }
 
  public abstract class Player<T> where T : class, new(){
   private static readonly Lazy<T> instance = new Lazy<T>(() => CreateInstanceofT());
 
+  private static T CreateInstanceofT(){
+   return Activator.CreateInstance(typeof(T), true) as T;
+  }
   public static T Instance{
    get{
     return instance.Value;
    }
-  }
-
-  private static T CreateInstanceofT(){
-   return Activator.CreateInstance(typeof(T), true) as T;
   }
  }
 
@@ -59,6 +53,19 @@ namespace TicTacToe {
  class y_Player : Player<y_Player>{
   public char letter = 'o';
  }
+
+ class Game{
+
+  static void Main(string[] args){
+   Grid grid = new Grid(3);
+   System.Console.WriteLine(grid[1,1]);
+   x_Player p1 = x_Player.Instance;
+   System.Console.WriteLine(p1.letter);
+   y_Player p2 = y_Player.Instance;
+   System.Console.WriteLine(p2.letter);
+
+   grid.turn(p1.letter,2,1);
+   System.Console.WriteLine(grid[2,1]);
+  }
+ }
 }
-
-
